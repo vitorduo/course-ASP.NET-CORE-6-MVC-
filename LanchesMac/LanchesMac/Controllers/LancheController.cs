827027733adb87927c04256f3cfcb185477848c6
vitorduo <vitorduo@outlook.com>
2022-07-25 23:03:35 +0100

    Incluindo um link e um button na exibição dos lanches

--- LanchesMac/LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/LanchesMac/Controllers/LancheController.cs
@@ -52,5 +52,11 @@ namespace LanchesMac.Controllers
 
             return View(lanchesListViewModel);
         }
+
+        public IActionResult Details(int lancheId)
+        {
+            var lanche = _lancheRepository.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+            return View(lanche);
+        }
     }
 }
