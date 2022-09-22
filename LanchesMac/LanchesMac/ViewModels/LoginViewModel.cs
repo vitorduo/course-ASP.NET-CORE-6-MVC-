7ec41063c0ebabf527552c4ba35fb9d439177271
vitorduo <vitorduo@outlook.com>
2022-09-22 17:54:43 +0100

    Implementando Login - View Model e Controller

---
+++ LanchesMac/LanchesMac/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace LanchesMac.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o nome")]
+        [Display(Name = "Usuário")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
