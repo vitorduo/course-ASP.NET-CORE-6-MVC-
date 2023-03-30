9feb92084728379c579d57b2a3a76c99bf2f7c52
vitorduo <vitorduo@outlook.com>
2023-03-30 21:06:02 +0100

    Paginação para lanches

--- LanchesMac/LanchesMac/Areas/Admin/Controllers/AdminPedidosController.cs
+++ LanchesMac/LanchesMac/Areas/Admin/Controllers/AdminPedidosController.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using LanchesMac.Context;
+﻿using LanchesMac.Context;
 using LanchesMac.Models;
 using Microsoft.AspNetCore.Authorization;
-using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ReflectionIT.Mvc.Paging;
 
 namespace LanchesMac.Areas.Admin.Controllers
@@ -27,35 +21,36 @@ namespace LanchesMac.Areas.Admin.Controllers
         // GET: Admin/AdminPedidos
         //public async Task<IActionResult> Index()
         //{
-        //      return View(await _context.Pedidos.ToListAsync());
+        //    return View(await _context.Pedidos.ToListAsync());
         //}
-
         public async Task<IActionResult> Index(string filter, int pageindex = 1, string sort = "Nome")
         {
             var resultado = _context.Pedidos.AsNoTracking()
-                .AsQueryable();
+                                      .AsQueryable();
 
-            if(!string.IsNullOrWhiteSpace(filter))
+            if (!string.IsNullOrWhiteSpace(filter))
             {
                 resultado = resultado.Where(p => p.Nome.Contains(filter));
             }
 
-            var model = await PagingList.CreateAsync(resultado, 5, pageindex, sort, "Nome");
+            var model = await PagingList.CreateAsync(resultado, 3, pageindex, sort, "Nome");
             model.RouteValue = new RouteValueDictionary { { "filter", filter } };
 
             return View(model);
         }
 
+
         // GET: Admin/AdminPedidos/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || _context.Pedidos == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
             var pedido = await _context.Pedidos
                 .FirstOrDefaultAsync(m => m.PedidoId == id);
+
             if (pedido == null)
             {
                 return NotFound();
@@ -89,7 +84,7 @@ namespace LanchesMac.Areas.Admin.Controllers
         // GET: Admin/AdminPedidos/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.Pedidos == null)
+            if (id == null)
             {
                 return NotFound();
             }
@@ -140,7 +135,7 @@ namespace LanchesMac.Areas.Admin.Controllers
         // GET: Admin/AdminPedidos/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.Pedidos == null)
+            if (id == null)
             {
                 return NotFound();
             }
@@ -160,23 +155,15 @@ namespace LanchesMac.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Pedidos == null)
-            {
-                return Problem("Entity set 'AppDbContext.Pedidos'  is null.");
-            }
             var pedido = await _context.Pedidos.FindAsync(id);
-            if (pedido != null)
-            {
-                _context.Pedidos.Remove(pedido);
-            }
-            
+            _context.Pedidos.Remove(pedido);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool PedidoExists(int id)
         {
-          return _context.Pedidos.Any(e => e.PedidoId == id);
+            return _context.Pedidos.Any(e => e.PedidoId == id);
         }
     }
 }
